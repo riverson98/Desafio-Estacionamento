f3df20aabac3a21af83c8f5763cfbb9fdbb113fe
riverson98 <riversonvicente@gmail.com>
2023-10-26 11:04:12 -0300

    Finalizacao do projeto sistema de estacionamento

---
+++ Estacionamento-riders/Models/Estacionamento.cs
@@ -0,0 +1,58 @@
+﻿namespace Estacionamento.Models;
+
+public class Estacionamento
+{
+    private Decimal precoInicial { get; set; }
+    private Decimal precoPorHora { get; set; }
+    private List<Veiculo> veiculos { get; set; }
+
+    public Estacionamento(decimal precoInicial, decimal precoPorHora)
+    {
+        this.precoInicial = precoInicial;
+        this.precoPorHora = precoPorHora;
+        this.veiculos = new List<Veiculo>();
+    }
+
+    public void AdicionaVeiculo()
+    {
+        Console.WriteLine("Digite a placa do veículo para estacionar:");
+        var placa = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Digite o modelo do veículo para estacionar:");
+        var modelo = Convert.ToString(Console.ReadLine());
+        var veiculo = new Veiculo(placa, modelo);
+        veiculos.Add(veiculo);
+    }
+
+    public void RemoveVeiculo()
+    {
+        Console.WriteLine("Digite a placa do veiculo que deseja retirar:");
+        var placa = Convert.ToString(Console.ReadLine());
+        var veiculo = veiculos.SingleOrDefault(veiculo => veiculo.placa.ToUpper().Equals(placa.ToUpper()));
+
+        if (veiculo != null)
+        {
+            Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+            var horas = Convert.ToDecimal(Console.ReadLine());
+            var valorTotal = precoInicial + precoPorHora * horas;
+            veiculos.Remove(veiculo);
+            Console.WriteLine($"O {veiculo.modelo} com a placa {veiculo.placa} foi removido e o preço total foi de: R$ {valorTotal}");
+        }
+        else
+            Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+    }
+
+    public List<Veiculo> ListaTodosOsVeiculos()
+    {
+        if (veiculos.Any())
+        {
+            Console.WriteLine("Os veículos estacionados são:");
+            foreach (var veiculo in veiculos)
+            {
+                Console.WriteLine($"{veiculo.modelo} - Placa: {veiculo.placa}");
+            }
+        }
+        else
+            Console.WriteLine("Não há veículos estacionados.");
+        return veiculos;
+    }
+}
---
+++ Estacionamento-riders/Models/Veiculo.cs
@@ -0,0 +1,13 @@
+﻿namespace Estacionamento.Models;
+
+public class Veiculo
+{
+    public string placa { get; set; }
+    public string modelo { get; set; }
+
+    public Veiculo(string placa, string modelo)
+    {
+        this.placa = placa;
+        this.modelo = modelo;
+    }
+}
